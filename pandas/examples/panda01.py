383e0c405b27ab41a2facfc63c8038ab6275f1dc
Sayan <sayanpal.iit@gmail.com>
2016-09-04 18:42:04 +0530

    What is this about

---
+++ pandas/examples/panda01.py
@@ -0,0 +1,14 @@
+#There are two manjor datastructures for panda
+# 1. Dataframe
+# 2. Series
+
+
+# We look at creating the panda dataframe and Series from variety of other python data structures
+
+#Creating a Panda from a Dictionary
+
+# From a List
+
+# From a Set
+
+# From a Tuple
